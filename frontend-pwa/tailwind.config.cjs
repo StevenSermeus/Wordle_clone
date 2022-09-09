47239d5b1ae849d7e936dd694a244e41a7c5788e
StevenSermeus <67290591+StevenSermeus@users.noreply.github.com>
2022-09-09 10:34:15 +0200

    Tailwind config update

--- frontend-pwa/tailwind.config.cjs
+++ frontend-pwa/tailwind.config.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],// or 'media' or 'class'
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
